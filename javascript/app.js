db4f6c5f0fa900e003a1bdb4719126ac636d4cbf
HumanJBooF <humanjboof@gmail.com>
2019-04-20 10:44:57 -0400

    es6 refactor

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,5 @@
 $(function () {
-    //game array filled with objects
-    var game = [
+    const game = [
         {
             question: 'According to Ron Swanson cats are... ',
             answers: ['Amazing', 'Better then dogs', 'Pointless', 'Stupid'],
@@ -49,65 +48,55 @@ $(function () {
             correct: 0,
             gif: './images/tom.gif'
         }];
-    //declaring variables for the rest of the game
-    var timer = 10;
-    var timerID;
-    var timerGo = false;
-    var unanswered = 0;
-    var right = 0;
-    var wrong = 0;
-    var currentQuestion = 0;
-    var userGuess = "";
-    var end = game.length;
 
-    //hiding the reset button
+    let timer = 10;
+    let timerID;
+    let timerGo = false;
+    let unanswered = 0;
+    let right = 0;
+    let wrong = 0;
+    let currentQuestion = 0;
+    let userGuess = "";
+    let end = game.length;
+
     $('.reset').hide();
 
-
-    console.log(game[currentQuestion].question);
-    //Setting up the timer for each question!
-    function startTimer() {
+    const startTimer = () => {
         if (!timerGo) {
             timerID = setInterval(countDown, 1000);
             timerGo = true;
         }
     }
-    //Countdown to get the timer to go down by 1
-    function countDown() {
-        $('.timer').html('<h2>Hurry up please... <br>' + timer + '</h2>') //Dipsplaying the timer on the page
-        timer--;
+
+    const countDown = () => {
+        $('.timer').html(`<h2>Hurry up please... <br>${timer}</h2>`)
         console.log(timer)
 
-        if (timer < 0) {   //if it hits 0
-            unanswered++;   //unanswered goes up 1
-            console.log(unanswered)
+        if (timer < 0) {
+            unanswered++;
             stopTimer();
             $('.answers').empty();
             $('.questions').empty();
             $('.timer').html('You Took to long! The Correct answer was ' + game[currentQuestion].answers[game[currentQuestion].correct]);
-            currentQuestion++;  //go to next index in game array
+            currentQuestion++;
             setTimeout(displayQuestion, 3000);
         }
     }
 
-    function stopTimer() {
+    const stopTimer = () => {
         timerGo = false;
-        timer = 10;  //resets the timer to 10
+        timer = 10;
         clearInterval(timerID);
     }
-    //display question with answers add 4 buttons for answers and gives them a value 0-3
-    function displayQuestion() {
-        //check if at end of game if not then go to next question      
-        if (resetGame()) {
 
-        } else {
-            var q = game[currentQuestion].question;
+    const displayQuestion = () => {
+        if (!resetGame()) {
+            let q = game[currentQuestion].question;
             $('.timer').empty();
             $('.questions').html(q);
             startTimer();
-            var answer = game[currentQuestion].answers;
+            let answer = game[currentQuestion].answers;
             for (i = 0; i < answer.length; i++) {
-                console.log(answer[i])
                 btn = $('<div>');
                 btn.text(answer[i]).addClass('button animated fadeInUp').attr('data-value', i)
                 $('.answers').append(btn);
@@ -115,65 +104,55 @@ $(function () {
         }
     }
 
-    //adds a gif to image div
-    function showGif() {
-        var gif = game[currentQuestion].gif;
-        console.log('gif test')
+    const showGif = () => {
+        let gif = game[currentQuestion].gif;
         $('.timer').empty();
-        $('.image').append('<img id="image" src=' + gif + '></img>')
+        $('.image').append(`<img id="image" src='${gif}'></img>`)
     }
 
 
-    function checkCorrect() {
-        //if userGuess is the correct answer
+    const checkCorrect = () => {
         if (userGuess === game[currentQuestion].correct) {
-            stopTimer(); //stop timer
-            right++;    //plus 1 to correct answers
-            
-            showGif();  //show Gif
-
+            stopTimer();
+            right++;
+            showGif();
             $('.answers').empty();
             $('.questions').empty();
-            currentQuestion++;      //Plus one to game index
-            setTimeout(function () {
-                $('#image').remove();     //removes gif
-                displayQuestion();      //Displays next question
-
+            currentQuestion++;
+            setTimeout(() => {
+                $('#image').remove();
+                displayQuestion();
             }, 3000);
-
         } else {
-            wrong++;    //wrong answers plus 1
-            userGuess = ""; //clear userGuess
-            stopTimer();    //stops timer
+            wrong++;
+            userGuess = "";
+            stopTimer();
             $('.answers').empty();
             $('.question').empty();
             $('.timer').empty();
-            //emptys divs and shows correct answer
             $('.showcur').text('Wrong! The correct answer is: ' + game[currentQuestion].answers[game[currentQuestion].correct])
-            currentQuestion++;       //plus one to game index
-            setTimeout(function () {
-                $('.showcur').empty();  //removes text
-                displayQuestion();      //next question!
+            currentQuestion++;
+            setTimeout(() => {
+                $('.showcur').empty();
+                displayQuestion();
             }, 3000);
         }
-        console.log(right)
-        console.log(wrong)
     }
 
 
-    function resetGame() {
+    const resetGame = () => {
         if (currentQuestion === end) {
             $('.questions').empty();
             $('.timer').empty();
-            $('.right').html('You got ' + right + ' Correct!')
-            $('.wrong').html('You got ' + wrong + ' Wrong!')
-            $('.unanswered').html('You forgot to answer ' + unanswered + ' questions')
+            $('.right').html(`You got ${right} Correct!`)
+            $('.wrong').html(`You got ${wrong} Wrong!`)
+            $('.unanswered').html(`You forgot to answer ${unanswered} questions`)
             $('.reset').show();
             return true;
         }
     }
-    //new game function that emptys all divs and resets starting screen
-    function newGame() {
+
+    const newGame = () => {
         $('.reset').hide();
         $('.answers').empty();
         $('.questions').empty();
@@ -184,14 +163,13 @@ $(function () {
         $('.start-pic').show();
         $('.start').show();
         $('.rules').show();
-        $('.wrapper').css({'border': '', 'background':'','box-shadow': ''})
+        $('.wrapper').css({ 'border': '', 'background': '', 'box-shadow': '' })
     }
 
 
-    //Click function hides start button and starting picture, starts timer and shows question.
     $('.start').on('click', function () {
         $(this).hide();
-        $('.wrapper').css({'border':'1pt solid black', 'background': '#E3DAC9', 'box-shadow': '0 0 75px black'});
+        $('.wrapper').css({ 'border': '1pt solid black', 'background': '#E3DAC9', 'box-shadow': '0 0 75px black' });
         $('.start-pic').hide();
         $('.rules').hide();
         currentQuestion = 0;
@@ -201,15 +179,13 @@ $(function () {
         startTimer();
         displayQuestion();
     })
-    //click function for the answers takes the value in the userGuess variable and goes to checkCorrect function
+
     $('.answers').on('click', '.button', function () {
         userGuess = parseInt($(this).attr('data-value'))
         checkCorrect();
     })
-    //reset button click event to start new game
+
     $('.reset').on('click', function () {
         newGame()
     })
-
-
 });
